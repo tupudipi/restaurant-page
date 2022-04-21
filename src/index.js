c180651d3b59b631488d734155892c48cc6199c7
tupudipi <tupudipi@gmail.com>
2022-04-21 23:09:33 +0300

    yes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+import './styles/home.css';
+
+let navButton = document.getElementById('borgir');
+navButton.addEventListener('click', displayNav);
+let navCloseBtn = document.getElementById('menu-close');
+navCloseBtn.addEventListener('click', closeNav);
+
+function displayNav() {
+    console.log('displayNav');
+    let nav = document.getElementById('nav-links');
+    nav.style.display = 'flex';
+    navCloseBtn.style.display = 'block';
+    navButton.style.display = 'none';
+}
+
+function closeNav() {
+    console.log('closeNav');
+    let nav = document.getElementById('nav-links');
+    nav.style.display = 'none';
+    navCloseBtn.style.display = 'none';
+    navButton.style.display = 'block';
+}
+
